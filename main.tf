13a537cf1e8f26504b4e5dcb7e542ecb83a8a20d
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:20:43 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network_gateway_nat_rule" "virtual_network_gateway_nat_rules" {
+  for_each = var.virtual_network_gateway_nat_rules
+
+  name                       = each.value.name
+  resource_group_name        = each.value.resource_group_name
+  virtual_network_gateway_id = each.value.virtual_network_gateway_id
+  ip_configuration_id        = each.value.ip_configuration_id
+  mode                       = each.value.mode
+  type                       = each.value.type
+
+  external_mapping {
+    address_space = each.value.external_mapping.address_space
+    port_range    = each.value.external_mapping.port_range
+  }
+
+  internal_mapping {
+    address_space = each.value.internal_mapping.address_space
+    port_range    = each.value.internal_mapping.port_range
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "virtual_network_gateway_nat_rules" {
+  description = <<EOT
+Map of virtual_network_gateway_nat_rules, attributes below
+Required:
+    - name
+    - resource_group_name
+    - virtual_network_gateway_id
+    - external_mapping (block):
+        - address_space (required)
+        - port_range (optional)
+    - internal_mapping (block):
+        - address_space (required)
+        - port_range (optional)
+Optional:
+    - ip_configuration_id
+    - mode
+    - type
+EOT
+
+  type = map(object({
+    name                       = string
+    resource_group_name        = string
+    virtual_network_gateway_id = string
+    ip_configuration_id        = optional(string)
+    mode                       = optional(string, "EgressSnat")
+    type                       = optional(string, "Static")
+    external_mapping = object({
+      address_space = string
+      port_range    = optional(string)
+    })
+    internal_mapping = object({
+      address_space = string
+      port_range    = optional(string)
+    })
+  }))
+}
+
