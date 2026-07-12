de6b01b04d596315c9d76ef2f7af8c6d7de81e57
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "virtual_network_gateway_nat_rules" {
-  description = "All virtual_network_gateway_nat_rule resources"
-  value       = azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules
-}
 output "virtual_network_gateway_nat_rules_external_mapping" {
-  description = "List of external_mapping values across all virtual_network_gateway_nat_rules"
-  value       = [for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : v.external_mapping]
+  description = "Map of external_mapping values across all virtual_network_gateway_nat_rules, keyed the same as var.virtual_network_gateway_nat_rules"
+  value       = { for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : k => v.external_mapping }
 }
 output "virtual_network_gateway_nat_rules_internal_mapping" {
-  description = "List of internal_mapping values across all virtual_network_gateway_nat_rules"
-  value       = [for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : v.internal_mapping]
+  description = "Map of internal_mapping values across all virtual_network_gateway_nat_rules, keyed the same as var.virtual_network_gateway_nat_rules"
+  value       = { for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : k => v.internal_mapping }
 }
 output "virtual_network_gateway_nat_rules_ip_configuration_id" {
-  description = "List of ip_configuration_id values across all virtual_network_gateway_nat_rules"
-  value       = [for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : v.ip_configuration_id]
+  description = "Map of ip_configuration_id values across all virtual_network_gateway_nat_rules, keyed the same as var.virtual_network_gateway_nat_rules"
+  value       = { for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : k => v.ip_configuration_id }
 }
 output "virtual_network_gateway_nat_rules_mode" {
-  description = "List of mode values across all virtual_network_gateway_nat_rules"
-  value       = [for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : v.mode]
+  description = "Map of mode values across all virtual_network_gateway_nat_rules, keyed the same as var.virtual_network_gateway_nat_rules"
+  value       = { for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : k => v.mode }
 }
 output "virtual_network_gateway_nat_rules_name" {
-  description = "List of name values across all virtual_network_gateway_nat_rules"
-  value       = [for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : v.name]
+  description = "Map of name values across all virtual_network_gateway_nat_rules, keyed the same as var.virtual_network_gateway_nat_rules"
+  value       = { for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : k => v.name }
 }
 output "virtual_network_gateway_nat_rules_resource_group_name" {
-  description = "List of resource_group_name values across all virtual_network_gateway_nat_rules"
-  value       = [for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : v.resource_group_name]
+  description = "Map of resource_group_name values across all virtual_network_gateway_nat_rules, keyed the same as var.virtual_network_gateway_nat_rules"
+  value       = { for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : k => v.resource_group_name }
 }
 output "virtual_network_gateway_nat_rules_type" {
-  description = "List of type values across all virtual_network_gateway_nat_rules"
-  value       = [for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : v.type]
+  description = "Map of type values across all virtual_network_gateway_nat_rules, keyed the same as var.virtual_network_gateway_nat_rules"
+  value       = { for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : k => v.type }
 }
 output "virtual_network_gateway_nat_rules_virtual_network_gateway_id" {
-  description = "List of virtual_network_gateway_id values across all virtual_network_gateway_nat_rules"
-  value       = [for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : v.virtual_network_gateway_id]
+  description = "Map of virtual_network_gateway_id values across all virtual_network_gateway_nat_rules, keyed the same as var.virtual_network_gateway_nat_rules"
+  value       = { for k, v in azurerm_virtual_network_gateway_nat_rule.virtual_network_gateway_nat_rules : k => v.virtual_network_gateway_id }
 }
 
